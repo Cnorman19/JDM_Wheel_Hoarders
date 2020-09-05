03db017d3748556b58056c9cdc672d5264c2d5df
cnorman <cnorm001@odu.edu>
2020-09-05 19:04:39 -0400

    Changed to profile.html

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm , UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def register(request):
@@ -24,4 +24,13 @@ def register(request):
 @login_required
 def profile(request):
 
-	return render(request, 'users/profile.html')
+	u_form = UserUpdateForm()
+	p_form = ProfileUpdateForm()
+
+	context = {
+
+		'u_form' : u_form,
+		'p_form' : p_form
+	}
+	
+	return render(request, 'users/profile.html', context)
